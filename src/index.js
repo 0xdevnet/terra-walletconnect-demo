75f96a8fd6a57455b9eda9366d1727318dac7bb6
0xWWW <topguru.plusplus@gmail.com>
2021-11-30 20:35:21 +0900

    update codebase structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+import {
+  NetworkInfo,
+  WalletProvider,
+  WalletStatus,
+  getChainOptions,
+} from "@terra-money/wallet-provider";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+
+const queryClient = new QueryClient();
+
+getChainOptions().then((chainOptions) => {
+  ReactDOM.render(
+    <StrictMode>
+      <WalletProvider {...chainOptions}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WalletProvider>
+    </StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
